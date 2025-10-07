4e87ff8533aab0459179faa87f8bd4bab5f0954a
aagincic <aleksandar.agincic@gmail.com>
2025-10-07 14:16:14 +0200

    initial commit

---
+++ linx.core.reporthandlerwpf/MappingAttributeWrapper.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace linx.core.reporthandlerwpf
+{
+    public class MappingAttributeWrapper
+    {
+        public PropertyInfo PropertyInfo { get; set; }
+        public LinxByteMappingAttribute LinxByteMapping { get; set; }
+    }
+}
